a491db3edd97c3d4db4540205581667d095a3c04
Isabel <34gomez.i@gmail.com>
2015-12-04 16:46:30 -0800

    GO's collide (Syringe button with the lip GameObjects)

---
+++ Assets/Scripts/ShipFiresBullets.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShipFiresBullets : MonoBehaviour {
+	public Button Lips;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+public void shoot () {
+	//CharacterController.detectCollisions
+	//Rigidbody2D.velocity
+	//CharacterController.OnControllerColliderHit(ControllerColliderHit hit)
+	}
+//public void CharacterController.SimpleMove() {
+public void OnCollisionEnter2D(Collision2D coll) {
+
+	if (coll.gameObject.tag == "Lip")
+    	print ("ApplyDamage");
+	}
+
+}
